3c89e732072af97ccbb8733c9e36490be0227ce0
Aron Rubin <aronrubin@gmail.com>
2020-12-01 13:27:10 -0500

    Refactored code into a library with an API.

--- examples/d6t-44l/d6t-44l.ino
+++ examples/d6t-44l/d6t-44l.ino
@@ -23,109 +23,41 @@
  */
 
 /* includes */
-#include <Wire.h>
+#include <Arduino.h>
+#include <OmronD6T.h>
 
-/* defines */
-#define D6T_ADDR 0x0A  // for I2C 7bit address
-#define D6T_CMD 0x4C  // for D6T-44L-06/06H, D6T-8L-09/09H, for D6T-1A-01/02
-
-#define N_ROW 4
-#define N_PIXEL (4 * 4)
-#define N_READ ((N_PIXEL + 1) * 2 + 1)
-
-uint8_t rbuf[N_READ];
-double ptat;
-double pix_data[N_PIXEL];
-
-uint8_t calc_crc(uint8_t data) {
-    int index;
-    uint8_t temp;
-    for (index = 0; index < 8; index++) {
-        temp = data;
-        data <<= 1;
-        if (temp & 0x80) {data ^= 0x07;}
-    }
-    return data;
-}
-
-/** <!-- D6T_checkPEC {{{ 1--> D6T PEC(Packet Error Check) calculation.
- * calculate the data sequence,
- * from an I2C Read client address (8bit) to thermal data end.
- */
-bool D6T_checkPEC(uint8_t buf[], int n) {
-    int i;
-    uint8_t crc = calc_crc((D6T_ADDR << 1) | 1);  // I2C Read address (8bit)
-    for (i = 0; i < n; i++) {
-        crc = calc_crc(buf[i] ^ crc);
-    }
-    bool ret = crc != buf[n];
-    if (ret) {
-        Serial.print("PEC check failed:");
-        Serial.print(crc, HEX);
-        Serial.print("(cal) vs ");
-        Serial.print(buf[n], HEX);
-        Serial.println("(get)");
-    }
-    return ret;
-}
-
-/** <!-- conv8us_s16_le {{{1 --> convert a 16bit data from the byte stream.
- */
-int16_t conv8us_s16_le(uint8_t* buf, int n) {
-    uint16_t ret;
-    ret = (uint16_t)buf[n];
-    ret += ((uint16_t)buf[n + 1]) << 8;
-    return (int16_t)ret;   // and convert negative.
-}
+OmronD6T d6t( OmronD6T::D6T_44L );
 
 /** <!-- setup {{{1 -->
- * 1. Initialize 
+ * 1. Initialize
        - initialize a Serial port for output.
 	   - initialize I2C.
  */
 void setup() {
-    Serial.begin(115200);  // Serial baudrate = 115200bps
+    Serial.begin( 115200 );  // Serial baudrate = 115200bps
     Wire.begin();  // i2c master
 	delay(620);
+    d6t.begin();
 }
 
 /** <!-- loop - Thermal sensor {{{1 -->
  * 2. read data.
  */
 void loop() {
-    int i = 0;
-	int16_t itemp = 0;
-	
-	// Read data via I2C
-	// I2C buffer of "Arduino MKR" is 256 buffer. (It is enough)
-    memset(rbuf, 0, N_READ);
-    Wire.beginTransmission(D6T_ADDR);  // I2C slave address
-    Wire.write(D6T_CMD);               // D6T register
-    Wire.endTransmission();
-	delay(1);
-    Wire.requestFrom(D6T_ADDR, N_READ);
-    while (Wire.available()) {
-        rbuf[i++] = Wire.read();
-    }
-    D6T_checkPEC(rbuf, N_READ - 1);
+    d6t.read();
+    // 1st data is PTAT measurement (: Proportional To Absolute Temperature)
+    Serial.print( "PTAT:" );
+    Serial.println( d6t.ambientTempC(), 1 );
 
-    //Convert to temperature data (degC)
-    ptat = (double)conv8us_s16_le(rbuf, 0) / 10.0;
-	for (i = 0; i < N_PIXEL; i++) {
-		itemp = conv8us_s16_le(rbuf, 2 + 2*i);
-		pix_data[i] = (double)itemp / 10.0;
-	}
-    
-    //Output results
-	Serial.print("PTAT:");
-    Serial.print(ptat, 1);
-    Serial.print(" [degC], Temperature: ");
-	for (i = 0; i < N_PIXEL; i++) {
-	    Serial.print(pix_data[i], 1);
-		Serial.print(", ");
-	}	
-    Serial.println(" [degC]");
-	
-    delay(300);	
+    // loop temperature pixels of each thrmopiles measurements
+    for (size_t row = 0, rows = d6t.rows(); row < rows; row++) {
+        Serial.print( d6t.objectTempF( 0, row ), 1 );  // print PTAT & Temperature
+        for (size_t col = 1, cols = d6t.cols(); col < cols; col++) {
+            Serial.print( "," );   // print delimiter
+            Serial.print( d6t.objectTempF( col, row ), 1 );  // print PTAT & Temperature
+        }
+        Serial.println( " [degC]" );  // wrap text at ROW end.
+    }
+    delay( 1000 );
 }
 // vi: ft=arduino:fdm=marker:et:sw=4:tw=80
